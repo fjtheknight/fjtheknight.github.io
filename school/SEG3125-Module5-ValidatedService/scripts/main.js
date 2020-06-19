486f20333d800a9ae92f059b4adb85ab78a6e0a4
firas-jribi <firas.jribi@cision.com>
2020-06-18 23:58:55 -0400

    leb5 seg3525

--- school/SEG3125-Module5-ValidatedService/scripts/main.js
+++ school/SEG3125-Module5-ValidatedService/scripts/main.js
@@ -1,50 +1,68 @@
 var experts = [
 	{
-		name : "heloo",
-		availability : {
-			monday: false,
-			tuesday: true,
-			wednsday: true,
-			thursday: true,
-			friday: false,
-			saturday: false,
-			sunday: false,
-		}
+		name : "Mark Aléatoire",
+		availability : [0,2,4]
 	},
 	{
-		name : "there",
-		availability : {
-			monday: true,
-			tuesday: true,
-			wednsday: true,
-			thursday: false,
-			friday: false,
-			saturday: false,
-			sunday: false,
-		}
+		name : "Alessandro Aléatoire",
+		availability : [0,1,2,3]
 	},
 	{
-		name : "wsgoin",
-		availability : {
-			monday: false,
-			tuesday: true,
-			wednsday: true,
-			thursday: true,
-			friday: true,
-			saturday: false,
-			sunday: false,
-		}
+		name : "Patricia Aléatoire",
+		availability : [1,2,3,4]
 	},
-
 
 ];
 
 
-//https://stackoverflow.com/questions/3446069/populate-dropdown-select-with-array-using-jquery
-function appendListOfExpertsToForm(){
-	var option = '<option disabled selected value> -- Selectionner un expert -- </option>';
+//https://www.w3schools.com/js/js_date_methods.asp
+//https://stackoverflow.com/questions/40762549/html5-input-type-date-onchange-event
+//https://stackoverflow.com/questions/3364493/how-do-i-clear-all-options-in-a-dropdown-box
+function updateExpertsList(e, id){
+	var dateEntered = new Date(e.target.value);
+    //console.log(dateEntered.getDay());
+    //console.log(id);
+
+    // using the function:
+	removeOptions($('#'+id)[0]);
+
+    var option = '<option disabled selected value> -- Selectionner un expert -- </option>';
+
+    var availableExperts = [];
 	for (var i=0;i<experts.length;i++){
-	   option += '<option value="'+ experts[i].name + '">' + experts[i].name + '</option>';
-	}
-	$('#veloderoute-select-expert').append(option);
+	   if(experts[i].availability.includes(dateEntered.getDay())){
+	   	availableExperts.push(experts[i].name);
+	   	option += '<option value="'+ experts[i].name + '">' + experts[i].name + '</option>';
+	   }
+	}   
+	$('#'+id).append(option);
 }
+
+function removeOptions(selectElement) {
+   var i, L = selectElement.options.length - 1;
+   for(i = L; i >= 0; i--) {
+      selectElement.remove(i);
+   }
+}
+
+
+
+// datepicker :/
+
+console.log('datepicker code');
+$('#datepicker').datepicker({
+    weekStart: 1,
+    daysOfWeekHighlighted: "6,0",
+    autoclose: true,
+    todayHighlight: true,
+});
+$('#datepicker').datepicker("setDate", new Date());
+
+
+
+// form submit
+$(document).ready(function(){
+  $("form").submit(function(){
+    alert("Réservation avec succès!");
+  });
+});
